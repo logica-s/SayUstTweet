0a3a3bd43b86e5a946ffa06969a9b11bc6c798a2
logimac <logimac@gmail.com>
2014-05-18 19:27:58 +0900

    一部のUnicode外字に対応、ｗｗｗをわらわらわらと読ませる機能などを追加
    グリフなどUnicodeの外字がツイート本文、もしくはスクリーンネームに存在すると、そのツイートは読み上げられない
    （Terminalへの出力はされる）問題について、一部の外字・絵文字を事前に削除することにより対応。
    ｗｗｗをわらわらわら、８８８をぱちぱちぱち、と読ませるの機能を追加

--- say-ust.py
+++ say-ust.py
@@ -8,32 +8,38 @@
 # require:  SayKotoeri, Saykana or SayKotoeri2
 # OS:       for Mac Only
 # Link:     [PythonでTwitterのタイムラインをストリーミングで読み上げさせるfor Mac[tweepy] « haya14busa](http://haya14busa.com/tweepy-read-aloud-tl/)
-# Link:     [tweepyでstreamingを使う - 備忘](http://monowasure78.hatenablog.com/entry/2013/11/26/tweepy%E3%81%A7streaming%E3%82%92%E4%BD%BF%E3%81%86
+# Link:     [tweepyでstreamingを使う - 備忘録](http://monowasure78.hatenablog.com/entry/2013/11/26/tweepy%E3%81%A7streaming%E3%82%92%E4%BD%BF%E3%81%86)
 # Link:     [tweepyでリアルタイムハッシュタグ検索 — 螺旋階段を一歩ずつ](http://hironow.bitbucket.org/blog/html/2014/01/18/tweepy_hashtag_search.html)
 
 import tweepy
 from subprocess import call
 import re
+import unicodedata
 import sys, codecs
 
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 sys.stdin = codecs.getreader('utf_8')(sys.stdin)
 
 def Setup():
-    global mode, name , replace_str
+    global mode, name, exception, replace_str
     
     # 0（ハッシュタグ）、1（メンション）
     mode = 0
     # ハッシュタグもしくはメンションの文字列を指定
     name = 'xxxxx'
+    # 自分自身のツイートを除外する場合、idを指定
+    # （除外しない場合は空白に）
+    exception = 'xxxxx'
     
     replace_str = [
     # 置換する文字列をカンマ区切りで指定
     'Twitter,ツイッター',
     'Ustream,ユーストリーム',
     'Ust,ユースト',
+    'pixiv,ピクシブ',
+    'Pixiv,ピクシブ',
     'Python,パイソン',
-    '読み上げ,よみあげ',
+    'bot,ボット',
     ]
     
     return Setup
@@ -51,7 +57,7 @@ def get_oauth():
 def str_replace(string):
     string = re.sub('&.+;', ' ', string)
     # Ustが追加する文字列を削除
-    string = re.sub('\([#@]' + name + '[^\)]+\)', '', string)
+    string = re.sub('\(\s?[#@]' + name + '[^\)]+\)', '', string)
     # remove URL
     string = re.sub('(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)', 'URL', string)
     # remove quote
@@ -61,20 +67,31 @@ def str_replace(string):
     
     string = re.sub('RT', 'Retweet', string)
     # ハッシュタグを削除
-    string = re.sub('#[0-9a-zA-Z_]{1,15}', '', string)  
+    string = re.sub('#[^#\s$]{1,15}', '', string)  
     # メンション、リプライを削除
-    string = re.sub('@[0-9a-zA-Z_]{1,15}', '', string) 
-    
+    string = re.sub('@[0-9a-zA-Z_]{1,15}', '', string)
+
+    # w（ワラ）、8（パチパチ）
+    if not (isinstance(string, unicode)):
+        string = unicode(string, 'utf-8')
+    string= string.replace('\u003d','=')
+    string = unicodedata.normalize('NFKC', string)
+    match = re.search('([^A-Za-z\s])[wW]', string, re.U)
+    if match != None:
+        buff = match.group(1)
+        string = re.sub('[^A-Za-z\s][wW]', buff + u'ワラ', string)
+    string = re.sub('[wW]{2,}', u'ワラワラワラ', string)
+    string = re.sub('8{3,}', u'ぱちぱちぱち', string)
+
     for buff in replace_str:
         list_value = buff.split(',')
-        string = re.sub(list_value[0],list_value[1], string)
-
+        string = string.replace(list_value[0],list_value[1])
     return string
 
 class CustomStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        if not hasattr(status, 'retweeted_status') :
+        if not hasattr(status, 'retweeted_status') or (status.author.screen_name == exception) :
             try:
                 print u'---{name}/@{screen}---\n   {text}\nvia {src} {created}'.format(
                         name = status.author.name,
@@ -83,6 +100,10 @@ class CustomStreamListener(tweepy.StreamListener):
                         src = status.source,
                         created = status.created_at)
                 read_text = str_replace(status.author.name.encode('utf-8')) + 'さん　' + str_replace(status.text.encode('utf-8'))
+                
+                #特殊文字を削除
+                read_text = re.sub(ur'[\u2600-\u2687\u2219-\u2761\ue468-\ue5df\uea80-\ueb88\ue63e-\ue6a5\ue6ac-\ue6ae\ue6b1-\ue6ba\ue6ce-\ue757\ue001-\ue05a\ue101-\ue15a\ue201-\ue253\ue301-\ue34d\ue401-\ue44c\ue501-\ue537%s\ue600-\ue619]', '', read_text)
+            
                 call(['SayKotoeri -s "-s 120" "{text}" >/dev/null 2>&1'.format(text=read_text)], shell=True)
 
             except Exception, e:
@@ -100,7 +121,7 @@ class CustomStreamListener(tweepy.StreamListener):
 def main():
     auth = get_oauth()
     Setup()
-    if not mode : str = '\u0040' + name
+    if not mode : str = '#' + name
     else : str = '@' + name
     stream = tweepy.Stream(auth, CustomStreamListener(), secure=True)
     stream.filter(languages=['ja'],track=[str])
